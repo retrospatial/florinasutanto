310503c8f7e6c783dc310efd2116bcbc48385aa9
retrospatial <florinasutanto@gmail.com>
2026-01-13 10:30:11 -0500

    styling

---
+++ src/lib/components/MoreAboutMe.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import md from '$lib/utils/md';
+
+	let content = page.data;
+</script>
+
+<div class="columns-3 gap-4">
+	<div class="border-2 border-accent-blue break-inside-avoid mb-4 p-4">
+		<h3 class="heading-3 uppercase">Sports</h3>
+		<p class="body">test</p>
+	</div>
+</div>
