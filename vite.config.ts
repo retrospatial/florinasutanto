ef36a2f5be8e03fdaa6fc0318404d4ee3d10288d
retrospatial <florinasutanto@gmail.com>
2026-01-22 16:21:05 -0500

    use vercel image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { enhancedImages } from '@sveltejs/enhanced-img';
 import yaml from '@rollup/plugin-yaml';
 
 export default defineConfig({
-	plugins: [enhancedImages(), tailwindcss(), sveltekit(), yaml()],
+	plugins: [tailwindcss(), sveltekit(), yaml()],
 	ssr: {
 		external: ['gray-matter', 'markdown-it', 'markdown-it-attrs', 'markdown-it-bracketed-spans', 'markdown-it-task-lists']
 	},
