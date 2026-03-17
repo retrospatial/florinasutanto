94a30bc2e22118f30eba0c29f9bfaad88fcea5be
retrospatial <florinasutanto@gmail.com>
2026-03-17 01:54:16 -0400

    added vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,8 +9,10 @@
 	import { page } from '$app/state';
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 
 	let { children } = $props();
 
