39d231dee359f3f998b418456b3acb66bc30fd5d
retrospatial <florinasutanto@gmail.com>
2026-07-29 10:45:21 -0400

    fix proxy rewrite

--- scripts/patch-vercel-config.js
+++ scripts/patch-vercel-config.js
@@ -3,27 +3,13 @@ import { readFileSync, writeFileSync } from 'fs';
 const configPath = '.vercel/output/config.json';
 const config = JSON.parse(readFileSync(configPath, 'utf-8'));
 
-const rewrites = [
-	{ src: '^/animorphs$', dest: 'https://retrospatial.github.io/animorphs/' },
-	{ src: '^/animorphs/(.*)$', dest: 'https://retrospatial.github.io/animorphs/$1' },
-	{ src: '^/tumblr-fandometrics$', dest: 'https://retrospatial.github.io/tumblr-fandometrics/' },
-	{
-		src: '^/tumblr-fandometrics/(.*)$',
-		dest: 'https://retrospatial.github.io/tumblr-fandometrics/$1'
-	},
-	{ src: '^/buffy-podcasts$', dest: 'https://retrospatial.github.io/buffy-podcasts/' },
-	{ src: '^/buffy-podcasts/(.*)$', dest: 'https://retrospatial.github.io/buffy-podcasts/$1' },
-	{
-		src: '^/html-comment-box-demo$',
-		dest: 'https://retrospatial.github.io/html-comment-box-demo/'
-	},
-	{
-		src: '^/html-comment-box-demo/(.*)$',
-		dest: 'https://retrospatial.github.io/html-comment-box-demo/$1'
-	}
-];
+const slugs = ['animorphs', 'tumblr-fandometrics', 'buffy-podcasts', 'html-comment-box-demo'];
 
-// Insert rewrites just before the catch-all route ("/.*")
+const rewrites = slugs.flatMap((slug) => [
+	{ src: `^/${slug}$`, headers: { Location: `/${slug}/` }, status: 308 },
+	{ src: `^/${slug}/(.*)$`, dest: `https://retrospatial.github.io/${slug}/$1` }
+]);
+
 const catchAllIndex = config.routes.findIndex((r) => r.src === '/.*');
 if (catchAllIndex !== -1) {
 	config.routes.splice(catchAllIndex, 0, ...rewrites);
